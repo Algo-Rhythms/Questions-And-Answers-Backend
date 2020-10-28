b5f8167943393116aaf9d278779a96292a2ed859
Kyle Graas <kgraas09@gmail.com>
2020-10-28 11:06:46 -0600

    Updated schema file

--- database/schema.sql
+++ database/schema.sql
@@ -1,11 +1,11 @@
 
-\c questions-and-answers;
+\c questionsandanswers;
 
 CREATE TABLE public.products
 (
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    name text COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT "PK_products" PRIMARY KEY (id)
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 999999999 CACHE 1 ),
+    name text COLLATE pg_catalog."default",
+    CONSTRAINT products_pkey PRIMARY KEY (id)
 )
 
 TABLESPACE pg_default;
@@ -13,19 +13,19 @@ TABLESPACE pg_default;
 ALTER TABLE public.products
     OWNER to postgres;
 
+
 CREATE TABLE public.questions
 (
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    product_id integer NOT NULL,
-    body text COLLATE pg_catalog."default" NOT NULL,
-    date_written date NOT NULL,
-    asker_name text COLLATE pg_catalog."default" NOT NULL,
-    asker_email text COLLATE pg_catalog."default" NOT NULL,
-    reported integer,
+    q_id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 999999999 CACHE 1 ),
+    body text COLLATE pg_catalog."default",
+    date_written date,
+    asker_name text COLLATE pg_catalog."default",
     helpful integer,
-    CONSTRAINT questions_pkey PRIMARY KEY (id),
-    CONSTRAINT "FKproduct_id" FOREIGN KEY (product_id)
+    reported integer,
+    asker_email text COLLATE pg_catalog."default",
+    product_id integer,
+    CONSTRAINT questions_pkey PRIMARY KEY (q_id),
+    CONSTRAINT product_id FOREIGN KEY (product_id)
         REFERENCES public.products (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
@@ -35,52 +35,71 @@ TABLESPACE pg_default;
 
 ALTER TABLE public.questions
     OWNER to postgres;
+-- Index: question_index
+
+-- DROP INDEX public.question_index;
+
+CREATE INDEX question_index
+    ON public.questions USING btree
+    (product_id ASC NULLS LAST)
+    TABLESPACE pg_default;
+
 
 CREATE TABLE public.answers
 (
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( CYCLE INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2000000000 CACHE 1 ),
-    body text COLLATE pg_catalog."default" NOT NULL,
-    date date NOT NULL,
-    answerer_name text COLLATE pg_catalog."default" NOT NULL,
-    helpfulness integer NOT NULL,
-    email text COLLATE pg_catalog."default" NOT NULL,
-    question_id integer NOT NULL,
+    a_id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 999999999 CACHE 1 ),
+    answer_body text COLLATE pg_catalog."default",
+    date date,
+    answerer_name text COLLATE pg_catalog."default",
+    helpfulness integer,
+    question_id integer,
     reported integer,
-    CONSTRAINT "PK_answers" PRIMARY KEY (id),
-    CONSTRAINT "FK_75" FOREIGN KEY (question_id)
-        REFERENCES public.questions (id) MATCH SIMPLE
+    email text COLLATE pg_catalog."default",
+    CONSTRAINT id PRIMARY KEY (a_id),
+    CONSTRAINT question_id FOREIGN KEY (question_id)
+        REFERENCES public.questions (q_id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
+        NOT VALID
 )
 
 TABLESPACE pg_default;
 
 ALTER TABLE public.answers
     OWNER to postgres;
+-- Index: answer_index
 
-CREATE INDEX "fkIdx_75"
+-- DROP INDEX public.answer_index;
+
+CREATE INDEX answer_index
     ON public.answers USING btree
     (question_id ASC NULLS LAST)
     TABLESPACE pg_default;
 
+
 CREATE TABLE public.photos
 (
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    url text COLLATE pg_catalog."default" NOT NULL,
-    answer_id integer NOT NULL,
-    CONSTRAINT "PK_photos" PRIMARY KEY (id),
-    CONSTRAINT "FK_78" FOREIGN KEY (answer_id)
-        REFERENCES public.answers (id) MATCH SIMPLE
+    p_id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 999999999 CACHE 1 ),
+    url text COLLATE pg_catalog."default",
+    answer_id integer,
+    CONSTRAINT "photos _pkey" PRIMARY KEY (p_id),
+    CONSTRAINT answer_id FOREIGN KEY (answer_id)
+        REFERENCES public.answers (a_id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
+        NOT VALID
 )
 
 TABLESPACE pg_default;
 
 ALTER TABLE public.photos
     OWNER to postgres;
+-- Index: photo_index
 
-CREATE INDEX "fkIdx_78"
+-- DROP INDEX public.photo_index;
+
+CREATE INDEX photo_index
     ON public.photos USING btree
     (answer_id ASC NULLS LAST)
     TABLESPACE pg_default;
+
